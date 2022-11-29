403f210fd48346c96826c70ce92b4764ea7f02ee
StanClaudiu <stanclaudiu321@gmail.com>
2022-11-29 13:26:16 +0200

    Queen moves

--- ChessBoard.py
+++ ChessBoard.py
@@ -13,6 +13,111 @@ class backgroundBoard:
         self.turn = 0 # meaning first one, easy to to 1-self.turn
     stack_all_transformations = list()
 
+    def find_pos_colision_Oy_Ox(self,f_pos_tuple):
+            if self.turn == 0 :
+                who_moves = "w"
+                who_stays = "b"
+            else :
+                who_moves = "b"
+                who_stays = "w"
+            f_row,f_col = f_pos_tuple
+            print(f_row,f_col)
+            # let's go up
+            current_row = f_row -1
+            while current_row >= 0 :
+                if self.board[current_row][f_col][0] == who_moves:
+                    break
+                if self.board[current_row][f_col][0] == who_stays:
+                    self.possible_current_moves.append((current_row,f_col))
+                    break
+                self.possible_current_moves.append((current_row,f_col))
+                current_row -= 1
+            # let's go down
+            current_row = f_row +1
+            while current_row <= 7 :
+                if self.board[current_row][f_col][0] == who_moves:
+                    break
+                if self.board[current_row][f_col][0] == who_stays:
+                    self.possible_current_moves.append((current_row,f_col))    
+                    break
+                self.possible_current_moves.append((current_row,f_col))
+                current_row += 1
+            # let's go right
+            current_col = f_col + 1
+            while current_col <= 7 :
+                if self.board[f_row][current_col][0] == who_moves:
+                    break
+                if self.board[f_row][current_col][0] == who_stays:
+                    self.possible_current_moves.append((f_row,current_col))
+                    break
+                self.possible_current_moves.append((f_row,current_col))
+                current_col += 1
+            # let's go left
+            current_col = f_col - 1
+            while current_col >= 0 :
+                if self.board[f_row][current_col][0] == who_moves:
+                    break
+                if self.board[f_row][current_col][0] == who_stays:
+                    self.possible_current_moves.append((f_row,current_col))
+                    break
+                self.possible_current_moves.append((f_row,current_col))
+                current_col -= 1
+            return len(self.possible_current_moves)!=0 # 0 mutari, atunci nu avem ce muta
+   
+    def find_pos_collision_diagonally(self,f_pos_tuple):
+        if self.turn == 0 :
+                who_moves = "w"
+                who_stays = "b"
+        else :
+                who_moves = "b"
+                who_stays = "w"
+        f_row,f_col = f_pos_tuple  
+        # let's go right-top
+        c_row,c_col = f_row - 1,f_col + 1
+        while c_row >=0 and c_col <=7 :
+            if self.board[c_row][c_col][0] == who_moves :
+                break
+            if self.board[c_row][c_col][0] == who_stays :
+                self.possible_current_moves.append((c_row,c_col))
+                break
+            self.possible_current_moves.append((c_row,c_col))
+            c_col += 1
+            c_row -= 1
+        # let's go left-bottom
+        c_row,c_col = f_row + 1,f_col - 1
+        while c_row <=7 and c_col >=0 :
+            if self.board[c_row][c_col][0] == who_moves :
+                break
+            if self.board[c_row][c_col][0] == who_stays :
+                self.possible_current_moves.append((c_row,c_col))
+                break
+            self.possible_current_moves.append((c_row,c_col))
+            c_col -= 1
+            c_row += 1
+        # let's go left-top
+        c_row,c_col = f_row - 1,f_col - 1
+        while c_row >=0 and c_col >= 0 :
+            if self.board[c_row][c_col][0] == who_moves :
+                break
+            if self.board[c_row][c_col][0] == who_stays :
+                self.possible_current_moves.append((c_row,c_col))    
+                break
+            self.possible_current_moves.append((c_row,c_col))
+            c_col -= 1
+            c_row -= 1
+        # let's go right-bottom
+        c_row,c_col = f_row + 1,f_col + 1
+        while c_row <=7 and c_col <= 7 :
+            if self.board[c_row][c_col][0] == who_moves :
+                break
+            if self.board[c_row][c_col][0] == who_stays :
+                self.possible_current_moves.append((c_row,c_col))
+                break
+            self.possible_current_moves.append((c_row,c_col))
+            c_col += 1
+            c_row += 1
+        return len(self.possible_current_moves) != 0
+            
 
     def valid_first_selection(self,pos_tuple):
         #here we are going to validate the first click if is alright, at the moment returns true
@@ -20,8 +125,8 @@ class backgroundBoard:
         (f_row,f_col) = pos_tuple
         valid_click = True # we believe everything good
         valid_click = self.verify_good_turn_color(pos_tuple) 
-        valid_click = self.verify_possible_move(pos_tuple)
-        print(valid_click)
+        valid_click = valid_click and self.verify_possible_move(pos_tuple)
+        print("Primul click este valid : ",valid_click)
         return valid_click
     
     def valid_second_selection(self,f_pos_tuple,s_pos_tuple):
@@ -31,7 +136,6 @@ class backgroundBoard:
         valid_click = True
 
         if valid_click:
-            print("I did smth")
             self.board[s_row][s_col] = self.board[f_row][f_col]
             self.board[f_row][f_col] = "--"
             self.turn = 1 - self.turn
@@ -47,7 +151,7 @@ class backgroundBoard:
 
         match piece_to_move[1]:
             case "K":
-                print("King")
+                print("King -------------- ")
                 all_the_same = True
                 for d_row in range(-1,2):
                     print(d_row)
@@ -60,7 +164,10 @@ class backgroundBoard:
                              all_the_same = False
                 valid_move = not all_the_same
             case "Q":
-                print("Queen")
+                print("Queen -----------")
+                valid_move = self.find_pos_colision_Oy_Ox((f_row,f_col)) or self.find_pos_collision_diagonally((f_row,f_col)) # let's see if we can move the queen
+                if valid_move :
+                    print("I can make the following moves : ",self.possible_current_moves)
             case "P":
                 print("Pioneer")
             case "C":
