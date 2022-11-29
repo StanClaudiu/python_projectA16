5c8094e9a6012aa33c7ea8c954866ed9fcc7c905
StanClaudiu <stanclaudiu321@gmail.com>
2022-11-29 14:23:21 +0200

    Remains the basic positions for the horse

--- ChessBoard.py
+++ ChessBoard.py
@@ -74,10 +74,12 @@ class backgroundBoard:
         f_row,f_col = f_pos_tuple  
         # let's go right-top
         c_row,c_col = f_row - 1,f_col + 1
+        print(self.board[c_row][c_col])
         while c_row >=0 and c_col <=7 :
             if self.board[c_row][c_col][0] == who_moves :
                 break
             if self.board[c_row][c_col][0] == who_stays :
+                print("Hit")
                 self.possible_current_moves.append((c_row,c_col))
                 break
             self.possible_current_moves.append((c_row,c_col))
@@ -118,6 +120,59 @@ class backgroundBoard:
             c_row += 1
         return len(self.possible_current_moves) != 0
             
+    def find_pos_basic_moves_pioneer(self,f_pos_tuple):
+        f_row,f_col = f_pos_tuple
+        if self.turn == 0 :
+            who_moves = "w"
+            who_stays = "b"
+        else :
+            who_moves = "b"
+            who_stays = "w"
+
+        # the white pioneer goes down
+        if who_moves == "w" :
+            if f_row < 7:
+                if  self.board[f_row + 1][f_col] == "--": #pos is free
+                    self.possible_current_moves.append((f_row+1,f_col))
+                if f_col < 7 and self.board[f_row + 1][f_col + 1][0] == who_stays: # enemy can be taken right_bottom
+                    self.possible_current_moves.append((f_row +1 , f_col + 1))
+                if f_col > 0 and self.board[f_row + 1][f_col - 1][0] == who_stays: # enemy can be taken left_bottom
+                    self.possible_current_moves.append((f_row +1 , f_col -1 ))
+        else:
+        # the black pionner goes up
+            if f_row > 0 :
+                if self.board[f_row -1][f_col] == "--": #pos is free
+                    self.possible_current_moves.append((f_row - 1,f_col))
+                if f_col < 7 and self.board[f_row - 1][f_col + 1][0] == who_stays: # enemy can be taken right_bottom
+                    self.possible_current_moves.append((f_row - 1 , f_col + 1))
+                if f_col > 0 and self.board[f_row -1][f_col - 1][0] == who_stays: # enemy can be taken left_bottom
+                    self.possible_current_moves.append((f_row - 1 , f_col -1 ))
+
+        return len(self.board)!=0
+        
+    def find_pos_basic_moves_king(self,f_pos_tuple):
+        if self.turn == 0 :
+            who_moves = "w"
+            who_stays = "b"
+        else :
+            who_moves = "b"
+            who_stays = "w"
+        f_row,f_col = f_pos_tuple
+        for d_line in range(-1,2):
+            for d_column in range(-1,2):
+                if d_line == 0 and d_column == 0 :
+                    continue
+                current_line = f_row + d_line
+                current_col = f_col + d_column
+                if 7>= current_line >= 0 and 7>= current_col >= 0 : # valid position
+                    if self.board[current_line][current_col] == "--":
+                        self.possible_current_moves.append((current_line,current_col))
+                    if self.board[current_line][current_col][0] == who_stays:
+                        self.possible_current_moves.append((current_line,current_col))
+        return len(self.possible_current_moves)!=0
+        
+        
+        
 
     def valid_first_selection(self,pos_tuple):
         #here we are going to validate the first click if is alright, at the moment returns true
@@ -134,6 +189,10 @@ class backgroundBoard:
         s_row,s_col = s_pos_tuple
         f_row,f_col = f_pos_tuple
         valid_click = True
+
+        #is the now pos good?, wihthout veryfing the chess condition
+
+        valid_click = (s_row,s_col) in self.possible_current_moves
 
         if valid_click:
             self.board[s_row][s_col] = self.board[f_row][f_col]
@@ -152,28 +211,31 @@ class backgroundBoard:
         match piece_to_move[1]:
             case "K":
                 print("King -------------- ")
-                all_the_same = True
-                for d_row in range(-1,2):
-                    print(d_row)
-                    for d_col in range(-1,2):
-                       neigh_row = f_row + d_row
-                       neigh_col = f_col + d_col
-                       if neigh_row >=0 and neigh_col >=0:
-                         if self.board[neigh_row][neigh_col][0] != who_moves: # possible moves
-                             self.possible_current_moves.append((neigh_row,neigh_col))
-                             all_the_same = False
-                valid_move = not all_the_same
+                valid_move = self.find_pos_basic_moves_king(f_pos_tuple)
+                if valid_move:
+                    print("King can move : ",self.possible_current_moves)
             case "Q":
                 print("Queen -----------")
-                valid_move = self.find_pos_colision_Oy_Ox((f_row,f_col)) or self.find_pos_collision_diagonally((f_row,f_col)) # let's see if we can move the queen
+                valid_moves_Ox_Oy = self.find_pos_colision_Oy_Ox(f_pos_tuple)
+                valid_moves_diagonally = self.find_pos_collision_diagonally(f_pos_tuple)
+                valid_move = valid_moves_Ox_Oy or valid_moves_diagonally
                 if valid_move :
                     print("I can make the following moves : ",self.possible_current_moves)
             case "P":
                 print("Pioneer")
+                valid_move_ = self.find_pos_basic_moves_pioneer(f_pos_tuple)
             case "C":
-                print("Castle")
+                print("Castle ---------")
+                valid_moves_Ox_Oy = self.find_pos_colision_Oy_Ox(f_pos_tuple)
+                valid_move = valid_moves_Ox_Oy
+                if valid_move:
+                    print("the moves : ",self.possible_current_moves)
             case "B":
                 print("Bishop")
+                valid_moves_diagonally = self.find_pos_collision_diagonally(f_pos_tuple)
+                valid_move = valid_moves_diagonally
+                if valid_move:
+                    print("the moves : ",self.possible_current_moves)
             case "H":
                 print("Horse")
         return valid_move
